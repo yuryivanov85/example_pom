c56dca5d6798687dc6eae2dc64a98ef5fc07ec07
Yury K <yuriyfromrussia@hotmail.com>
2021-09-02 20:05:41 -0400

    initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+# import allure
+import pytest
+# from allure_commons.types import AttachmentType
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture()
+def browser():
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    # maximize browser window to full screen
+    # browser.maximize_window()
+    yield browser
+    # make a screenshot before closing the browser
+    # allure.attach(browser.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
+    # when test is done, close ALL windows of the browser
+    browser.quit()
+
